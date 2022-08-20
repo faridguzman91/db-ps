64c3da72662ab53e5e8e286c8b17a7d4791f4f37
faridguzman91 <farid.guzman91@hotmail.com>
2022-08-20 17:07:16 +0200

    fixed some bs server.js mysql2 crap

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import mysql from 'mysql2'
+import colors from 'colors';
+import dotenv from 'dotenv';
+import express from 'express';
+import mysql from 'mysql2';
+
+dotenv.config();
 
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.port || 5000
 
+const connectPscale = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to Pscale!')
+    // const {errorHandler} = require('.middleware/errorMiddleware')
+    // const connection = require("mysql2/typings/mysql/lib/Connection")
+    // connection.end()
 
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log('connected to pscale db!')
-
-connection.end()
-
-dotenv.config()
-
-const app = express()
+const app = express();
 
 app.listen(3001, () => {
-    console.log('Server is listening on port 3001')
+    console.log('app listening')
 })
