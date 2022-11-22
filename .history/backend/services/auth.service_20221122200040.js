bb9e5d850a0db43ed0c325a14226ac5241df320c
faridguzman91 <farid.guzman91@hotmail.com>
2022-11-22 20:04:09 +0100

    jwt bcrypt auth / services folder

---
+++ .history/backend/services/auth.service_20221122200040.js
@@ -0,0 +1,10 @@
+import {
+    PrismaClient
+} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+require("dotenv").config();
+
+const bcrypt = require("bcrypt");
+const jwt = require("../utils/webtokens");
---
+++ .history/backend/services/auth.service_20221122200148.js
@@ -0,0 +1,55 @@
+import {
+    PrismaClient
+} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+require("dotenv").config();
+
+const bcrypt = require("bcrypt");
+const jwt = require("../utils/webtokens");
+
+class AuthService {
+    static async registerUser(req, res) {
+        const {
+            email,
+            password
+        } = req.body;
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    email: email,
+                },
+            });
+            if (user) {
+                return res.status(409).json({
+                    status: false,
+                    message: "User already exists",
+                });
+            }
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const newUser = await prisma.user.create({
+                data: {
+                    email: email,
+                    password: hashedPassword,
+                },
+            });
+            const accessToken = await jwt.signAccessToken({
+                email: newUser.email,
+            });
+            const refreshToken = await jwt.signRefreshToken({
+                email: newUser.email,
+            });
+            res.status(201).json({
+                status: true,
+                message: "User created successfully",
+                data: {
+                    accessToken,
+                    refreshToken,
+                },
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
